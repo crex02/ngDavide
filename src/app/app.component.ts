9812e5d3e41e1c7f3ccfd226a7c68a266210057d
Pentakilj97 <127298119+Pentakilj97@users.noreply.github.com>
2023-05-10 13:22:44 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
   title = 'davide';
+  public firstName : string = 'Diego';
+
+
+  constructor(){
+    console.log('davide');
+    
+  }
+
+  ngOnInit(){
+    console.log('nhOninit');
+    
+  }
+  ngAfterViewInit():void {
+    console.log('ngAfterViewInit');
+    
+  }
+  saySomething() {
+    alert ('botton pressed');
+  }
 }
